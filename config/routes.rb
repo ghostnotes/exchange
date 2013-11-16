61bbeaa836dd1aecfa882fda82cac9924a9395fb
ghostnotes <ghostnotesdot@gmail.com>
2013-11-16 20:05:58 +0900

    Added haml settings and rake test.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Exchange::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'currency#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ lib/tasks/currency_exchange_rate.rake
@@ -0,0 +1,5 @@
+namespace :currency_exchange_rate do
+  task :test => :environment do
+    puts 'am testing the task of exchange...'
+  end
+end
